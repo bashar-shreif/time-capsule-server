d1032df6948f43a8d505a2042a603f76dab41d01
Bashar Shreif <basharchreif70@gmail.com>
2025-07-17 11:43:52 +0300

    Creating Migrations

---
+++ database/migrations/2025_07_16_235516_capsules.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capsules', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('privacy_settings_id');
+            $table->integer('reveal_mode_id');
+            $table->integer('mood_id');
+            $table->integer('country_id');
+            $table->text('message');
+            $table->timestamps();
+            $table->timestamp('revealed_at');
+        });
+        Schema::create('moods', function (Blueprint $table) {
+            $table->id();
+            $table->string('mood');
+            $table->timestamps();
+        });
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->timestamps();
+        });
+        Schema::create('reveal_modes', function (Blueprint $table) {
+            $table->id();
+            $table->string('reveal_mode');
+            $table->timestamps();
+        });
+        Schema::create('privacy_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('privacy_setting');
+            $table->timestamps();
+        });
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
